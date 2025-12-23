cb3091c71807e59d6ca62eca3a9a4989d18a36a8
Romuald DUCONSEIL <romualdduconseil@camera.home>
2025-12-23 12:03:16 +0100

    Add some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <img
+          className="picture"
+          src="src/assets/picture.png"
+          alt="Maka Art profile picture"
+        />
+        <img className="logo" src="src/assets/logo.png" alt="Maka Art logo" />
+        <div className="description">
+          <h1>🇫🇷 Anime Paint Artist</h1>
+          <h2>🇫🇷 Have a wonderful day – Maka Art ❤️</h2>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a className="button">Materials I use</a>
+            </li>
+            <li>
+              <a className="button">Commission info</a>
+            </li>
+            <li>
+              <a className="button">Social links</a>
+            </li>
+            <li>
+              <a className="button">Support me (ko-fi)</a>
+            </li>
+            <li>
+              <a className="button">PayPal.me</a>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer>
+        <p>Made with ♥️... and a keyboard!</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
